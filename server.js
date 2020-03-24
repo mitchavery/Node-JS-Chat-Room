69b5e84db16afd6abf62f9db973f60d18077c5f2
mitchavery <avery.mitch651@gmail.com>
2020-03-24 15:44:25 -0700

    set up server

---
+++ server.js
@@ -0,0 +1,18 @@
+const path = require("path");
+const http = require("http");
+const express = require("express");
+const socketio = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+app.use(express.static(path.join(__dirname, "public")));
+
+io.on("connection", socket => {
+  console.log("New WS Connection...");
+});
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT} baby`));
