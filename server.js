40d0e35fa8c40cbd98f5dc03d992addc8c54f3c6
mitchavery <avery.mitch651@gmail.com>
2020-03-24 16:04:40 -0700

    configured main.js:

--- server.js
+++ server.js
@@ -10,7 +10,6 @@ const io = socketio(server);
 app.use(express.static(path.join(__dirname, "public")));
 
 io.on("connection", socket => {
-
   socket.emit("message", "Welcome to ChatCord");
   socket.broadcast.emit("message", "A user has joined the chat");
   socket.on("disconnect", () => {
